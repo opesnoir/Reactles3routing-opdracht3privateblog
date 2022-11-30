57938328b7490bc86fcbd0fcbc40997b92ae1598
opesnoir <goodsurroundsme@outlook.com>
2022-11-30 09:50:42 +0100

    navigatie jsx opgezet

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import styles from "./Navigation.module.css";
 
 function Navlink(props) {
     return null;
@@ -8,36 +9,41 @@ function Navlink(props) {
 function Navigation(props) {
     return (
         <>
-            <ul>
-                <li>
-                    <NavLink
-                        className={({ isActive }) => isActive ? 'active-menu-link' : 'default-menu-link'}
-                        to={"/"}>
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
-                        to={"/login"}>
-                        Login
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
-                        to={"/blogposts"}>
-                        Blog overzicht
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
-                        to={"/blogposts/:blogId"}>
-                        Blog post
-                    </NavLink>
-                </li>
-            </ul>
+            <nav className={styles["navigationContainer"]}>
+                <ul className={styles["navigationContent"]}><li className={styles["navigationItem"]}><NavLink
+                            className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
+                            to={"/"}>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
+                            to={"/login"}>
+                            Login
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
+                            to={"/blogposts"}>
+                            Blog overzicht
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
+                            to={"/blogposts/:blogId"}>
+                            Blog post
+                        </NavLink>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <button className={styles["navigationButtonItem"]}>Login</button>
+                    </li>
+                </ul>
+            </nav>
         </>
     );
 }
